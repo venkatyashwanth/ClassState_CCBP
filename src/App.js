a2013b7e5bc3494e704a407b4bff1d827f279663
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-01-28 12:03:56 +0530

    Build Counter using class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Welcome from "./components/Welcome"
 import Counter from './components/Counter';
+
 const App = () => {
   return (
     <>
-      <Welcome name="Venkat"/>
-      <Counter name="Yashwanth"/>
+      <Counter/>
     </>
   )
 
