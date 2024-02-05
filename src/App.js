0ff58883c760453e20af13c9b0246282f27e13d0
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-02-05 07:00:59 +0530

    Auth button with if logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-import Counter from './components/Counter';
+import { Component } from 'react';
+// import Counter from './components/Counter';
 
-const App = () => {
-  return (
-    <>
-      <Counter/>
-    </>
-  )
+class App extends Component {
+  state = { isLoggedIn: true }
 
+  renderAuthButton = () => {
+    const { isLoggedIn } = this.state;
+    if (isLoggedIn === true) {
+      return <button>Logout</button>
+    }
+    return <button>Login</button>
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        {this.renderAuthButton()}
+      </div>
+    )
+  }
 }
+
+// const App = () => {
+//   return (
+//     <>
+//       <Counter/>
+//     </>
+//   )
+// }
 export default App 
